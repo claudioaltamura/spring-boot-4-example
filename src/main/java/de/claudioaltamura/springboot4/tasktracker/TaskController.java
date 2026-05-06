b20af9874bccc995d20f7d046ebb1d8d14999469
claudioaltamura <claudio@claudioaltamura.de>
2026-05-06 17:18:26 +0200

    chg: add @tag

--- src/main/java/de/claudioaltamura/springboot4/tasktracker/TaskController.java
+++ src/main/java/de/claudioaltamura/springboot4/tasktracker/TaskController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(name = "Tasks", description = "Task management endpoints")
 public class TaskController {
 
     private final TaskService service;
